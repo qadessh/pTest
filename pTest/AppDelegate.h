9e58d1abcb6955c8e26526a5ab47ffef0c5533e1
Marcel Chirila <marcel.chirila@gmail.com>
2013-08-07 18:21:54 +0300

    v1

---
+++ pTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  pTest
+//
+//  Created by XGRoup5 on 8/7/13.
+//  Copyright (c) 2013 XGRoup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
